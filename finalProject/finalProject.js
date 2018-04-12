e1de7ce5326d7197d1c6837f502de42f1aaaef71
karan95 <kmthakor95@gmail.com>
2018-04-11 22:39:21 -0400

    CG final project update

--- finalProject/finalProject.js
+++ finalProject/finalProject.js
@@ -1,49 +1,94 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+/*global THREE*/
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-var modal = document.getElementById('canvas');
-modal.appendChild(renderer.domElement);
+var sceneWidth;
+var sceneHeight;
+var camera;
+var scene;
+var renderer;
+var dom;
+var hero;
+var sun;
+var ground;
+var orbitControl;
 
-var cubegeometry = new THREE.CubeGeometry(1, 1, 1);
-var cubematerial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x000000 });
+init();
+function init() {
+	// set up the scene
+	createScene();
 
-var cube = new THREE.Mesh(cubegeometry, cubematerial);
+	//call game loop
+	update();
+}
 
-scene.add(cube);
+function createScene(){
+    sceneWidth=window.innerWidth;
+    sceneHeight=window.innerHeight;
+    scene = new THREE.Scene();//the 3d scene
+    //scene.fog = new THREE.Fog(0x00ff00, 50, 800);//enable fog
+    camera = new THREE.PerspectiveCamera( 60, sceneWidth / sceneHeight, 0.1, 1000 );//perspective camera
+    renderer = new THREE.WebGLRenderer({alpha:true});//renderer with transparent backdrop
+    renderer.shadowMap.enabled = true;//enable shadow
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.setSize( sceneWidth, sceneHeight );
+    dom = document.getElementById('TutContainer');
+	dom.appendChild(renderer.domElement);
+	
+	//add items to scene
+	var heroGeometry = new THREE.BoxGeometry( 1, 1, 1 );//cube
+	var heroMaterial = new THREE.MeshStandardMaterial( { color: 0xB13E0F } );
+	hero = new THREE.Mesh( heroGeometry, heroMaterial );
+	hero.castShadow=true;
+	hero.receiveShadow=false;
+	hero.position.y=2;
+	scene.add( hero );
+	var planeGeometry = new THREE.PlaneGeometry( 5, 5, 4, 4 );
+	var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x2E9AFE } )
+	ground = new THREE.Mesh( planeGeometry, planeMaterial );
+	ground.receiveShadow = true;
+	ground.castShadow=false;
+	ground.rotation.x=-Math.PI/2;
+	scene.add( ground );
 
-camera.position.z = 5;
+	camera.position.z = 5;
+	camera.position.y = 1;
+	
+	sun = new THREE.DirectionalLight( 0xffffff, 0.8);
+	sun.position.set( 0,4,1 );
+	sun.castShadow = true;
+	scene.add(sun);
+	//Set up shadow properties for the sun light
+	sun.shadow.mapSize.width = 256;
+	sun.shadow.mapSize.height = 256;
+	sun.shadow.camera.near = 0.5;
+	sun.shadow.camera.far = 50 ;
+	
+	orbitControl = new THREE.OrbitControls( camera, renderer.domElement );//helper to rotate around in scene
+	orbitControl.addEventListener( 'change', render );
+	//orbitControl.enableDamping = true;
+	//orbitControl.dampingFactor = 0.8;
+	orbitControl.enableZoom = false;
+	
+	//var helper = new THREE.CameraHelper( sun.shadow.camera );
+	//scene.add( helper );// enable to see the light cone
+	
+	window.addEventListener('resize', onWindowResize, false);//resize callback
+}
 
-var spheregeometry = new THREE.SphereGeometry(0.8, 16, 16);
-var spherematerial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x000000 });
-var sphere = new THREE.Mesh(spheregeometry, spherematerial);
-
-sphere.position.set(-2.0, 0, 0);
-
-// scene.add(sphere);
-
-var cylindergeometry = new THREE.CylinderGeometry(0.6, 0.6, 2, 50, false);
-var cylindermaterial = new THREE.MeshLambertMaterial({ wireframe: true, color: 0x000000 });
-var cylinder = new THREE.Mesh(cylindergeometry, cylindermaterial);
-
-cylinder.position.set(2.0, 0, 0);
-scene.add(cylinder);
-
-var render = function () {
-    requestAnimationFrame(render);
-
-    cube.rotation.y += 0.01;
-    cube.rotation.x += 0.01;
-    cube.rotation.z += 0.01;
-
-    sphere.rotation.y += 0.01;
-
-    cylinder.rotation.y += 0.01;
-    cylinder.rotation.x += 0.01;
-    cylinder.rotation.z += 0.01;
-
-    renderer.render(scene, camera);
-};
-
-render();
+function update(){
+    //animate
+    hero.rotation.x += 0.01;
+    hero.rotation.y += 0.01;
+    render();
+	requestAnimationFrame(update);//request next update
+}
+function render(){
+    renderer.render(scene, camera);//draw
+}
+function onWindowResize() {
+	//resize & align
+	sceneHeight = window.innerHeight;
+	sceneWidth = window.innerWidth;
+	renderer.setSize(sceneWidth, sceneHeight);
+	camera.aspect = sceneWidth/sceneHeight;
+	camera.updateProjectionMatrix();
+}
